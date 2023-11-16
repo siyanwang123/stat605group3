a6d5fe4e7aeb84eba86ffc0c22b34b104c250af8
VivianBeagle <147095922+VivianBeagle@users.noreply.github.com>
2023-11-16 14:33:17 -0600

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -14,3 +14,9 @@ cat comedies/* histories/* poetry/* tragedies/* > ../all_plays.txt
 # Break the large file into 5 smaller files outside the shakespeare folder
 cd ..
 split -n l/5 --additional-suffix=.txt all_plays.txt chunk_
+
+mv file_aa.txt file1.txt
+mv file_ae.txt file2.txt
+mv file_ab.txt file3.txt
+mv file_ac.txt file4.txt
+mv file_ad.txt file5.txt
